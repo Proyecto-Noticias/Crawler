83ce1fd0c21a6b127494d510569c6ac50b22966d
Julian Santos <ingjuliansantos@gmail.com>
2020-10-24 07:31:27 -0500

    [ADD] Added API Endpoints

--- news_crawler_scraper/main.py
+++ news_crawler_scraper/main.py
@@ -7,6 +7,22 @@ app = FastAPI()
 
 
 @app.post("/eltiempo")
-def execute_spider():
+def execute_eltiempo_spider():
     os.system('python go_spider_eltiempo.py')
-    return {"Hello": "World"}
+    return {"Status": "El Tiempo has been scraped!"}
+
+@app.post("/lanacion")
+def execute_lanacion_spider():
+    os.system('python go_spider_lanacion.py')
+    return {"Status": "La Nacion has been scraped!"}
+
+@app.post("/eluniversal")
+def execute_eluniversal_spider():
+    os.system('python go_spider_eluniversal.py')
+    return {"Status": "El Universal has been scraped!"}
+
+@app.post("/xataka")
+def execute_xataka_spider():
+    os.system('python go_spider_xataka.py')
+    return {"Status": "Xataka has been scraped!"}
+
